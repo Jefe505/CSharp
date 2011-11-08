8598a9529b6d1a4bc5c91ea509ff0c92eb85ffab
Nisse Smorkster <smorkster@gmail.com>
2011-11-09 00:12:45 +0100

    Adding VS 2010 Exp

---
+++ Learning5/Backup/Learning5/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Learning5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void closeBtn_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void openBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
