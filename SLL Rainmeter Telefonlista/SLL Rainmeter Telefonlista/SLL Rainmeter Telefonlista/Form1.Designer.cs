cf8dd3bb3f282ecd9f57745ee7b51e8abf9741be
Nisse Smorkster <smorkster@gmail.com>
2012-06-29 21:53:23 +0200

    Adding new program

---
+++ SLL Rainmeter Telefonlista/SLL Rainmeter Telefonlista/SLL Rainmeter Telefonlista/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace SLL_Rainmeter_Telefonlista
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tab2ndline = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPersonal = new System.Windows.Forms.TabPage();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.listaPersonal = new System.Windows.Forms.ListBox();
+            this.listaHP = new System.Windows.Forms.ListBox();
+            this.tab2ndline.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPersonal.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tab2ndline
+            // 
+            this.tab2ndline.Controls.Add(this.tabPage1);
+            this.tab2ndline.Controls.Add(this.tabPersonal);
+            this.tab2ndline.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.tab2ndline.Location = new System.Drawing.Point(0, 48);
+            this.tab2ndline.Name = "tab2ndline";
+            this.tab2ndline.SelectedIndex = 0;
+            this.tab2ndline.Size = new System.Drawing.Size(898, 406);
+            this.tab2ndline.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.listaHP);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(890, 380);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Helpdesk / 2nd Line";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPersonal
+            // 
+            this.tabPersonal.Controls.Add(this.listaPersonal);
+            this.tabPersonal.Location = new System.Drawing.Point(4, 22);
+            this.tabPersonal.Name = "tabPersonal";
+            this.tabPersonal.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPersonal.Size = new System.Drawing.Size(890, 380);
+            this.tabPersonal.TabIndex = 1;
+            this.tabPersonal.Text = "Personal";
+            this.tabPersonal.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 29);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Spara lista";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(112, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(94, 29);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Ändra post";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(212, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(94, 29);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Skapa RM lista";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(799, 12);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(94, 29);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Stäng";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // listaPersonal
+            // 
+            this.listaPersonal.FormattingEnabled = true;
+            this.listaPersonal.Location = new System.Drawing.Point(0, 3);
+            this.listaPersonal.Name = "listaPersonal";
+            this.listaPersonal.Size = new System.Drawing.Size(888, 368);
+            this.listaPersonal.TabIndex = 0;
+            // 
+            // listaHP
+            // 
+            this.listaHP.FormattingEnabled = true;
+            this.listaHP.Location = new System.Drawing.Point(0, 4);
+            this.listaHP.Name = "listaHP";
+            this.listaHP.Size = new System.Drawing.Size(889, 368);
+            this.listaHP.TabIndex = 0;
+            this.listaHP.SelectedIndexChanged += new System.EventHandler(this.listaHP_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(898, 454);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tab2ndline);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tab2ndline.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPersonal.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tab2ndline;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPersonal;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ListBox listaHP;
+        private System.Windows.Forms.ListBox listaPersonal;
+    }
+}
+
---
+++ SLL Rainmeter Telefonlista/SLL Rainmeter Telefonlista/SLL Rainmeter Telefonlista/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Text;
+using System.IO;
+using System.Xml;
+
+namespace SLL_Rainmeter_Telefonlista
+{
+    public class MainProgram
+    {
+        public void YourMethod(String namn, String hsaid, String intern, String mobil)
+        {
+            // Do something with strFirst and strLast.
+            Console.WriteLine("{0}, {1}, {2}, {3}", namn, hsaid, intern, mobil);
+            return;
+        }
+        public void ReadXML(String xmlText)
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.LoadXml(xmlText);
+            // alternately, _doc.Load( _strFilename); to read from a file.
+            XmlNodeList namn = doc.GetElementsByTagName("namn");
+            XmlNodeList hsaid = doc.GetElementsByTagName("hsaid");
+            XmlNodeList intern = doc.GetElementsByTagName("interntel");
+            XmlNodeList mobil = doc.GetElementsByTagName("mobiltel");
+            // I'm assuming every FirstName has a LastName in this example, your requirements may vary. // 
+            for (int i = 0; i < namn.Count; ++i)
+            {
+                FillList(namn[i].InnerText, hsaid[i].InnerText, intern[i].InnerText, mobil[i].InnerText);
+            }
+
+            return;
+        }
+
+        private void FillList(string namn, string hsaid, string intern, string mobil)
+        {
+            
+        }
+
+        private void WriteRMInit()
+        {
+            for (int i = 0; i < 0; i++)
+                CreateRMSkin(namn[i].InnerText, hsaid[i].InnerText, intern[i].InnerText, mobil[i].InnerText);
+        }
+        public void CreateRMSkin(String namn, String hsaid, String intern, String mobil)
+        {
+            CopyInit();
+            TextWriter writer = new StreamWriter("Personal.ini");
+            String[] meters = new String[3];
+
+            meters = CreateMeterGroup(namn, hsaid, intern, mobil));
+            for (int i=0; i < 3; i++)
+            {
+                writer.WriteLine(meters[i]);
+            }
+            writer.Close();
+            return;
+        }
+        private String[] CreateMeterGroup(String namn, String hsaid, String intern, String mobil)
+        {
+            int i=0;
+            String[] a = new String[3];
+            String[] X = new String[] {"0", "200r" , "166r"};
+            String[] W = new String[] {"200", "166" , "134"};
+            String meterConfig1 = "]\\Meter=String\\X=";
+            String meterConfig2 = "\\Y=0R\\H=20\\W=";
+            String meterConfig3 = "\\MeterStyle=TextStyle\\SolidColor=#bakgrund.ColorTitel#\\StringStyle=#font.StyleTitel#\\FontSize=#font.SizeTitel#\\MouseOverAction=!SetOptionGroup " + hsaid + " SolidColor #bakgrund.ColorIn\\MouseLeaveAction=!SetOptionGroup " + hsaid + " SolidColor #bakgrund.Color##\\Group=" + hsaid + "\\Text=""";
+
+       	    a[0] = "[metText" + hsaid + meterConfig1 + X[i] + meterConfig2 + W + meterConfig3 + namn + "\"";
+            i++;
+       	    a[1] = "[metText" + hsaid + meterConfig1 + X[i] + meterConfig2 + W + meterConfig3 + intern + " - " + hsaid + "\"";
+            i++;
+       	    a[2] = "[metText" + hsaid + meterConfig1 + X[i] + meterConfig2 + W + meterConfig3 + mobil + "\"";
+
+            return a;
+        }
+        public void CopyInit()
+        {
+            StreamReader readPersonal = new StreamReader("PersonalRMInit.txt");
+            StreamWriter writePersonal = new StreamWriter("Personal.ini");
+
+            writePersonal.WriteLine(readPersonal.ReadLine());
+
+            writePersonal.Close();
+            readPersonal.Close();
+            return;
+        }
+        public static void Main(String[] args)
+        {
+            XMLApp _app = new XMLApp();
+            // Passing XML text as a String, you can also use the
+            // XMLDocument::Load( ) method to read the XML from a file.
+            _app.ReadXML(@"<personal><person><namn>Carl Landberg</namn><hsaid>6G1W</hsaid><interntel>785 92</interntel><mobiltel>073-702 44 88</mobiltel></person><person><namn>Alexander Ratajc</namn><hsaid>6740</hsaid><interntel>785 71</interntel><mobiltel>070 479 91 78</mobiltel></person></personal>");
+        }
+    }
+}
